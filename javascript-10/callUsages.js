e4ec5e69fca563f263b76428b72f2b6cf897fe0f
inna-i <iivashchuk@talend.com>
2024-08-15 12:12:50 +0200

    feat: add call usages example

--- javascript-10/callUsages.js
+++ javascript-10/callUsages.js
@@ -1,9 +1,14 @@
 function Product(name, price) {
     this.name = name;
     this.price = price;
+    this.id = 'id-' + name;    
 
     this.getPrice = function() {
         console.log(`Price is ${this.price} euro`);
+    }
+
+    this.getId = function() {
+        console.log(this.id)
     }
 }
 
@@ -13,8 +18,19 @@ function Food(name, price) {
     this.category = 'food';
 }
 
+function Clothes(name, price) {
+    Product.call(this, name, price)
+
+    this.category = 'clothes';
+}
+
 const drinks = new Food('drinks', 5);
 console.log(drinks.category);
 console.log(drinks.price);
 
 drinks.getPrice();
+
+const jeans = new Clothes('jeans', 100);
+
+jeans.getId()
+jeans.getPrice()
